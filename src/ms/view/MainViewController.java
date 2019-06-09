51ad93ebbac1a18d7666c9b7c924b600a9418db4
olivier <ozyra@laposte.net>
2019-06-09 16:29:28 +0200

    commit 2
    -Création de l'interface V1.

--- src/ms/view/MainViewController.java
+++ src/ms/view/MainViewController.java
@@ -7,10 +7,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
 
 public class MainViewController implements Initializable {
 
+	
+	@FXML private MenuItem newEasyButton;
+	@FXML private MenuItem newNormButton;
+	@FXML private MenuItem newHardButton;
+	@FXML private MenuItem quitButton;
+	
 	@FXML private Button startButton;
 	@FXML private Label timerLabel;
 	@FXML private Label mineLabel;
